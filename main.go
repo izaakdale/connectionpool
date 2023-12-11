4d5e7dd650cfbbe1ca939cb97c14e7852589db52
Izaak Dale <izaakdaledev@gmail.com>
2023-12-11 00:02:52 -0800

    little refactor

--- main.go
+++ main.go
@@ -1,33 +1,40 @@
 package main
 
 import (
+	"log"
 	"net"
 	"time"
 )
 
 const (
-	N_CONNS  = 1
+	N_CONNS  = 3
 	CONN_TTL = 3 * time.Second
 )
 
 func main() {
-	pool, err := NewConnectionPool(N_CONNS, CONN_TTL)
+	pool, err := NewConnectionPool("tcp", "localhost:8080", N_CONNS, CONN_TTL)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	go func() {
 		for {
 			conn := pool.WaitForConnection()
-			conn.Write([]byte("hello from goroutine\n"))
-			time.Sleep(time.Second)
+			_, err := conn.Write([]byte("hello from goroutine\n"))
+			if err != nil {
+				log.Fatal(err)
+			}
+			time.Sleep(3 * time.Second)
 			pool.ReleaseConnection(conn)
 		}
 	}()
 
 	for {
 		conn := pool.WaitForConnection()
-		conn.Write([]byte("hello from main\n"))
+		_, err := conn.Write([]byte("hello from main\n"))
+		if err != nil {
+			log.Fatal(err)
+		}
 		time.Sleep(time.Second)
 		pool.ReleaseConnection(conn)
 	}
@@ -35,13 +42,25 @@ func main() {
 
 type Connection struct {
 	ID         int
-	tcp        net.Conn
+	network    string
+	address    string
+	netStream  net.Conn
 	lastActive time.Time
 }
 
 func (c *Connection) Write(b []byte) (n int, err error) {
 	c.lastActive = time.Now()
-	return c.tcp.Write(b)
+	return c.netStream.Write(b)
+}
+
+func (c *Connection) Read(b []byte) (n int, err error) {
+	c.lastActive = time.Now()
+	return c.netStream.Read(b)
+}
+
+func (c *Connection) Close() error {
+	c.lastActive = time.Now()
+	return c.netStream.Close()
 }
 
 type ConnectionPool struct {
@@ -49,14 +68,14 @@ type ConnectionPool struct {
 	Connections   chan *Connection
 }
 
-func NewConnectionPool(nConns int, connTTL time.Duration) (*ConnectionPool, error) {
+func NewConnectionPool(network string, address string, nConns int, connTTL time.Duration) (*ConnectionPool, error) {
 	pool := ConnectionPool{
 		Connections:   make(chan *Connection, nConns),
 		ConnectionTTL: connTTL,
 	}
 
 	for i := 0; i < nConns; i++ {
-		err := pool.NewConnection(i)
+		err := pool.NewConnection(i, network, address)
 		if err != nil {
 			return nil, err
 		}
@@ -64,19 +83,19 @@ func NewConnectionPool(nConns int, connTTL time.Duration) (*ConnectionPool, erro
 	return &pool, nil
 }
 
-func (p *ConnectionPool) NewConnection(id int) error {
-	conn, err := net.Dial("tcp", "localhost:8080")
+func (p *ConnectionPool) NewConnection(id int, network string, address string) error {
+	conn, err := net.Dial(network, address)
 	if err != nil {
 		return err
 	}
-	p.Connections <- &Connection{id, conn, time.Now()}
+	p.Connections <- &Connection{id, network, address, conn, time.Now()}
 	return nil
 }
 
 func (p *ConnectionPool) WaitForConnection() *Connection {
 	conn := <-p.Connections
 	if time.Since(conn.lastActive) > p.ConnectionTTL {
-		p.NewConnection(conn.ID)
+		p.NewConnection(conn.ID, conn.network, conn.address)
 		conn = <-p.Connections
 	}
 	return conn
